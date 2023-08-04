df20b16236708ec9ad728439ee68dec995da6e38
Abdul Azis <abdul.azizz1108@gmail.com>
2023-08-04 15:42:27 +0700

    fix playlist object result

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -7,16 +7,26 @@ class PlaylistsService {
 
   async getPlaylistWithSongs (userId, playlistId) {
     const query = {
-      text: `SELECT playlists.name, users.username, 
-        songs.id, songs.title, songs.performer FROM playlists 
-        LEFT JOIN users ON users.id=playlists.owner 
-        LEFT JOIN songs ON songs.playlist_id=playlists.id 
+      text: `SELECT playlists.name, songs.id, songs.title, songs.performer FROM playlists
+        LEFT JOIN songs ON songs.playlist_id=playlists.id
         WHERE playlists.id=$1 AND playlists.owner=$2`,
-      values: [userId, playlistId]
+      values: [playlistId, userId]
     }
 
     const result = await this._pool.query(query)
-    return result.rows
+
+    const { name } = result.rows[0]
+    const playlistResult = { id: playlistId, name }
+    const songsResult = result.rows.map(({ id, title, performer }) => ({
+      id, title, performer
+    }))
+
+    return {
+      playlist: {
+        ...playlistResult,
+        songs: songsResult.length !== 1 || songsResult[0].id != null ? songsResult : []
+      }
+    }
   }
 }
 
